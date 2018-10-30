fc0cee014c0a5e8dbaeb678f96a3e8d8484fc4b4
Clinton Dreisbach <clinton@dreisbach.us>
2018-10-30 12:17:01 -0400

    Updates from W1D2

---
+++ w2/examples/stone_to_lbs/lbs_to_stone.py
@@ -0,0 +1,3 @@
+pounds = float(input("How many pounds? "))
+stone = pounds / 14
+print(pounds, "pounds is", stone, "stone")
---
+++ w2/examples/stone_to_lbs/stone_to_lbs.py
@@ -0,0 +1,3 @@
+stone = float(input("How many stone? "))
+pounds = stone * 14
+print(stone, "stone is", pounds, "pounds")
