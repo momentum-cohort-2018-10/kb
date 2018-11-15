dd350958ec39df30c08d5d21a48ce11559dc7a00
Clinton Dreisbach <clinton@dreisbach.us>
2018-11-15 11:44:11 -0500

    Add Dog model

--- w3/examples/shelter/django/pets/admin.py
+++ w3/examples/shelter/django/pets/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from pets.models import Dog
+
 
 # Register your models here.
+class DogAdmin(admin.ModelAdmin):
+    model = Dog
+    list_display = (
+        'name',
+        'description',
+        'age',
+    )
+
+
+admin.site.register(Dog, DogAdmin)
--- w3/examples/shelter/django/pets/models.py
+++ w3/examples/shelter/django/pets/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.CharField(max_length=255, null=False, blank=False)
+    img_filename = models.CharField(max_length=50, null=True, blank=True)
+    age = models.CharField(
+        max_length=10,
+        null=False,
+        blank=False,
+        choices=(
+            ('puppy', 'Puppy'),
+            ('young', 'Young'),
+            ('adult', 'Adult'),
+            ('senior', 'Senior'),
+        ))
+    size = models.IntegerField(
+        null=False,
+        blank=False,
+        choices=(
+            (1, 'Tiny'),
+            (2, 'Small'),
+            (3, 'Medium'),
+            (4, 'Large'),
+        ))
+    good_with_kids = models.BooleanField(default=False)
+    good_with_dogs = models.BooleanField(default=False)
+    good_with_cats = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
