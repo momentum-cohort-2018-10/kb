6910259d297032a8ed27d1b23b3f5da742bb5d6a
Clinton Dreisbach <clinton@dreisbach.us>
2018-11-16 13:59:01 -0500

    Make pet search form work

--- w3/examples/shelter/django/pets/forms.py
+++ w3/examples/shelter/django/pets/forms.py
@@ -5,10 +5,14 @@ from pets.models import DOG_AGE_CHOICES, DOG_SIZE_CHOICES
 class SearchForm(forms.Form):
     age = forms.ChoiceField(
         label="Age",
-        choices=((None, '---'), ) + DOG_AGE_CHOICES,
+        widget=forms.widgets.RadioSelect,
+        choices=((None, "Any age"), ) + DOG_AGE_CHOICES,
         required=False)
     size = forms.MultipleChoiceField(
-        label="Size", choices=DOG_SIZE_CHOICES, required=False)
+        widget=forms.widgets.CheckboxSelectMultiple,
+        label="Size",
+        choices=DOG_SIZE_CHOICES,
+        required=False)
     good_with_kids = forms.BooleanField(label="Good with kids", required=False)
     good_with_dogs = forms.BooleanField(
         label="Good with other dogs", required=False)
